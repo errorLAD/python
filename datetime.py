f801978ae6e50b49107dfccebb798f8d35645051
Abhishek Mishra <ab.mishra@yahoo.com>
2018-01-17 16:46:51 +0530

    Create datetime.py

---
+++ datetime.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+presentime = datetime.now()
+print("NOW THE TIME :",presentime)
+print("TOdays date is:", presentime.strftime('%y-%m-%d'))
+print("year is:",presentime.year)
+print("MOnth is ",presentime.month)
+print("Day is :",presentime.day)
