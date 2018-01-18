dc556fce83b4ef64b509be5557d8f361de197996
Abhishek Mishra <ab.mishra@yahoo.com>
2018-01-18 18:34:05 +0530

    Create min-mx.py

---
+++ min-mx.py
@@ -0,0 +1,9 @@
+student ={
+         'biswal':100,
+         'niraj':200,
+         'sam':150,
+          'gokul':50,
+          'amar':56,
+}
+print(min(zip(student.values(),student.keys())))
+print(max(zip(student.values(),student.keys())))
